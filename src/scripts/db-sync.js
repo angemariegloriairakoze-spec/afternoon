0c01b0277e3f1b54e2eadfc9972ead36b8ca8711
Ikundabayo Placide <ikundabayoplacide500@gmail.com>
2026-04-09 19:05:38 +0200

    modified codes for having user model/user table

---
+++ src/scripts/db-sync.js
@@ -0,0 +1,20 @@
+import sequelize from "../config/db.js";
+import { createUserTable } from "../database/migrations/users.js";
+import"../database/models/index.js";
+
+const syncDatabase=async()=>{
+    try {
+        console.log("database starting async....");
+        await sequelize.authenticate();
+        console.log("database connection established successfully");
+        await createUserTable();
+        await sequelize.sync({logging:false});
+         console.log("Database synced successfully 🔥🔥🔥🔥🔥🔥");
+         process.exit(0);
+    } catch (error) {
+        console.log("Failed to sync database",error);
+        process.exit(1);
+    }
+};
+
+syncDatabase();
